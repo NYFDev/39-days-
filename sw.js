ca8734edff90d331ffe5a94c628df7c2f221f35f
NYF Dev <Nyfholdings@gmail.com>
2026-08-25 02:23:50 -0600

    Add NYF offline service worker

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'nyf-public-v1';
+const CORE = [
+  '/',
+  '/assets/nyf-public-20260814.css',
+  '/assets/nyf-mvp-20260814.css',
+  '/assets/nyf-public-20260814.js',
+  '/assets/nyf-app-icon.svg',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(hit => hit || caches.match('/'))));
+    return;
+  }
+
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    if (response.ok) {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  })));
+});
