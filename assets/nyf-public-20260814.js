124ef1ec7ec69de8f1a6d5c7f623100199d0dbd8
NYF Dev <Nyfholdings@gmail.com>
2026-08-25 02:24:08 -0600

    Register NYF web app service worker

--- assets/nyf-public-20260814.js
+++ assets/nyf-public-20260814.js
@@ -167,3 +167,9 @@ if (newsletterForm && newsletterStatus) {
     }
   });
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {});
+  });
+}
